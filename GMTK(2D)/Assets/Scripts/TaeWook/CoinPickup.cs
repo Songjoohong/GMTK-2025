77572754e4b8095b794727e33dd44d8a2dbd24c0
Kim-TaeWook <131635471+Kim-TaeWook@users.noreply.github.com>
2025-08-03 17:29:13 +0900

    코인 중복 스코어 해결

--- GMTK(2D)/Assets/Scripts/TaeWook/CoinPickup.cs
+++ GMTK(2D)/Assets/Scripts/TaeWook/CoinPickup.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    private bool isCollected = false;  // 한 번만 처리하도록 체크
     private CoinManager coinManager;
 
     public void SetCoinManager(CoinManager manager)
@@ -11,16 +12,18 @@ public class CoinPickup : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (isCollected) return;  // 이미 처리되었으면 무시
+
         if (other.CompareTag("Player") || other.CompareTag("Ghost"))
         {
             if(other.CompareTag("Player"))
                 SoundManager.Instance.PlayOneShotSound("Coin");
-            if (coinManager != null) 
-            {
-                coinManager.AddScore();
-            }
 
-            Destroy(gameObject);
+            isCollected = true;  // 점수 처리 막기 위해 플래그 설정
+            if (coinManager != null)
+                coinManager.AddScore();
+
+            Destroy(transform.gameObject); // 코인 오브젝트 제거
         }
     }
 }
